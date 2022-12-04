52d81181ccae6e43e3a38a37cb2c3be7638aee78
Arron <arron.m@spacetime.co.nz>
2022-12-05 11:36:15 +1300

    pinyin and english definition

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ const {
   candidateCommonStrokes,
   targetNumCharacters,
   targetNumStrokes,
-  radicalHints
+  radicalHints,
+  pinyin,
+  defs
 } = config
 
 function App () {
   const [paths, setPaths] = useState([])
   const [chosen, setChosen] = useState([])
   const [guessCount, setGuessCount] = useState(0)
-  const [success, setSuccess] = useState(false)
 
   /*
   useEffect(() => {
@@ -53,6 +54,7 @@ function App () {
     return targetStrokes - numStrokes
   })
   const totalRemaining = remaining.reduce((a, b) => a + b, 0)
+  const success = totalRemaining <= 0
 
   return (
     <div className='App'>
@@ -65,60 +67,63 @@ function App () {
         //title='上字'
         titleStyle={{ fontSize: '4rem' }}
       >
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div
-            style={{
-              justifyContent: 'space-between',
-              flex: '1',
-              padding: '0px 15%'
-            }}
-          >
-            {candidates.map(candidate => (
-              <svg
-                key={candidate}
-                viewBox='0 0 1024 1024'
-                style={{
-                  aspectRatio: '1/1',
-                  border: '1px solid #fefefe',
-                  borderRadius: '4px',
-                  height: '30px',
-                  margin: '8px'
-                }}
-                onClick={() => {
-                  setPaths(existing => [
-                    ...existing,
-                    candidateCommonStrokes[candidate]
-                  ])
-                  setChosen(existing => [...existing, candidate])
-                  setGuessCount(g => (g += 1))
-                }}
-              >
-                <g transform='scale(1, -1) translate(0, -900)'>
-                  {[
-                    ...candidateStrokes[candidate].map((path, index) => ({
-                      stroke: path,
-                      index
-                    })),
-                    ...(chosen.includes(candidate)
-                      ? candidateCommonStrokes[candidate].reverse
-                          .flat()
-                          .map(x => ({ ...x, match: true }))
-                      : [])
-                  ].map(({ stroke, match, index }) => (
-                    <path
-                      className={`path ${match ? 'pathRadical' : ''}`}
-                      style={{
-                        opacity: chosen.includes(candidate) && !match ? 0.1 : 1
-                      }}
-                      key={index}
-                      d={stroke}
-                    ></path>
-                  ))}
-                </g>
-              </svg>
-            ))}
+        {!success && (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div
+              style={{
+                justifyContent: 'space-between',
+                flex: '1',
+                padding: '0px 15%'
+              }}
+            >
+              {candidates.map(candidate => (
+                <svg
+                  key={candidate}
+                  viewBox='0 0 1024 1024'
+                  style={{
+                    aspectRatio: '1/1',
+                    border: '1px solid #fefefe',
+                    borderRadius: '4px',
+                    height: '30px',
+                    margin: '8px'
+                  }}
+                  onClick={() => {
+                    setPaths(existing => [
+                      ...existing,
+                      candidateCommonStrokes[candidate]
+                    ])
+                    setChosen(existing => [...existing, candidate])
+                    setGuessCount(g => (g += 1))
+                  }}
+                >
+                  <g transform='scale(1, -1) translate(0, -900)'>
+                    {[
+                      ...candidateStrokes[candidate].map((path, index) => ({
+                        stroke: path,
+                        index
+                      })),
+                      ...(chosen.includes(candidate)
+                        ? candidateCommonStrokes[candidate].reverse
+                            .flat()
+                            .map(x => ({ ...x, match: true }))
+                        : [])
+                    ].map(({ stroke, match, index }) => (
+                      <path
+                        className={`path ${match ? 'pathRadical' : ''}`}
+                        style={{
+                          opacity:
+                            chosen.includes(candidate) && !match ? 0.1 : 1
+                        }}
+                        key={index}
+                        d={stroke}
+                      ></path>
+                    ))}
+                  </g>
+                </svg>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
 
         <div
           style={{
@@ -163,16 +168,28 @@ function App () {
             ))}
           </div>
         </div>
-        <p>{guessCount > 0 && guessCount.toLocaleString('zh-u-nu-hanidec')}</p>
         {
-          <p style={{ fontSize: '1rem' }}>
-            {totalRemaining <= 0
-              ? 'complete!'
-              : `${totalRemaining} stroke${
-                  totalRemaining > 1 ? 's' : ''
-                } remaining`}
+          <p style={{ flex: '1', fontSize: '1rem' }}>
+            {success ? (
+              <>
+                <h2>{pinyin}</h2>
+                <>
+                  {defs.map(d => (
+                    <span key={d}>{d}</span>
+                  ))}
+                </>
+              </>
+            ) : (
+              `${totalRemaining} stroke${
+                totalRemaining > 1 ? 's' : ''
+              } remaining`
+            )}
           </p>
         }
+        <p style={{ flex: '0', fontSize: '1rem' }}>
+          {guessCount > 0 && guessCount.toLocaleString('zh-u-nu-hanidec')}
+        </p>
+
         <p style={{ flex: '0', fontSize: '1rem', color: '#bbb' }}>
           by{' '}
           <a
